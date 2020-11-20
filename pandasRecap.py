32f5292bd6cc6e94bdb05c94884cdee4aa0c20c6
Jitgitbit <rowowolf@gmail.com>
2020-11-20 12:53:04 +0100

    DataFrames

--- pandasRecap.py
+++ pandasRecap.py
@@ -23,3 +23,19 @@ b = pd.Series(z)
 print(b)
 print("----------------------------------")
 print()
+print('DataFrames, frames of Series: ')
+print("----------------------------------")
+A = [1,2,3,4]
+B = [5,6,7,8]
+C = [9,0,1,2]
+D = [3,4,5,6]
+E = [7,8,9,0]
+print('df = pd.DataFrame(data, index or rows, columns)')
+df = pd.DataFrame([A,B,C,D,E], ['a','b','c','d','e'], ['W','X','Y','Z'])
+print()
+print(df)
+print()
+print('cmd + ctrl + spacebar')
+print('😃')
+print()
+print("----------------------------------")
